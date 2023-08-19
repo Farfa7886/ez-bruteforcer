e3af90bb9d846fdbd1c416961eefbe82b2a8300f
farfa <farfa7886@gmail.com>
2023-08-19 18:18:42 +0200

    thanks for 100 downloads in 18 hours!

--- test/cjs/index.js
+++ test/cjs/index.js
@@ -2,17 +2,20 @@ const bruteforce = require("ez-bruteforcer");
 
 const charactersList = "abcdefghijkl!mnopqrstuvwxyz ";
 const secretPassword = "hello world!";
-const totalCombs = bruteforce.getDispositionsNumber(charactersList, 6, 1);
-let currentIndex = 0;
+const totalCombs = bruteforce.getDispositionsNumber(charactersList, 12, 12);
+// let currentIndex = 0;
 bruteforce.runSync({
   data: charactersList,
   step: (attempt) => {
-    currentIndex++;
-    console.log(`Trying password (${currentIndex}/${totalCombs}): ${attempt}`);
+    // currentIndex++;
+    // console.log(`Trying password (${currentIndex}/${totalCombs}) : ${attempt}`);
     if (attempt == secretPassword) {
       console.log(`> Found password: ${attempt}`);
       return true;
-    } else return false;
+    } else {
+      process.stdout.write(`Trying password (${totalCombs}) : ${attempt}\n`);
+      return false;
+    }
   },
   finish: () => {
     console.log("> Nothing has been found:(");
